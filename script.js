324fa84c5893854a2768a2cd570d06bca978cac0
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-08-24 18:06:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,23 +3,15 @@ var modal = document.querySelector('.modal');
 
 // Get the button that opens the modal
 var btn = document.getElementById("openModal");
-
-// Get the <span> element that closes the modal
-var span = document.querySelector(".close-modal");
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
+var closeBtn=document.querySelector(".close-modal");
+btn.addEventListener('click', function() {
   modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+});
+closeBtn.addEventListener('click', function() {
   modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+});
+window.addEventListener('click', function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+});
