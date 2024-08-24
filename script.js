7575cb788ba9aaee5908a9236b5854f21cc2b8b2
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-08-24 17:12:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+var modal=document.querySelector('.modal');
+var btn=document.getElementById("openModal");
+var span=document.querySelector(".close-modal");
+btn.onclick=function(){
+	modal.style.dispay="block";
+}
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
