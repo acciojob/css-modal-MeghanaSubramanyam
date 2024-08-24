071eefb9f8f31ae158f78cdd7f6bcf0a0cc49b41
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2024-08-24 18:18:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-// Get the modal
-var modal = document.querySelector('.modal');
+document.addEventListener('DOMContentLoaded', function() {
+    var modal = document.querySelector('.modal');
+    var btn = document.getElementById("openModal");
+    var closeBtn = document.querySelector(".close-modal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("openModal");
-var closeBtn=document.querySelector(".close-modal");
-btn.addEventListener('click', function() {
-  modal.style.display = "block";
-});
-closeBtn.addEventListener('click', function() {
-  modal.style.display = "none";
-});
-window.addEventListener('click', function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    btn.addEventListener('click', function() {
+        modal.style.display = "block";
+    });
+
+    closeBtn.addEventListener('click', function() {
+        modal.style.display = "none";
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    });
 });
